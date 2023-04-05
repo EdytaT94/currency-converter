86612146e8ba20bae69d3c1a7f1e72889c137418
Edyta Trzebska <edytatrzebska@gmail.com>
2023-04-05 14:03:34 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+console.log("Witam wszystkich!");
+
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-form__pln");
+let currencyElement = document.querySelector(".js-form__currency");
+let resultElement = document.querySelector(".js-form__sum");
+
+let rateEUR = 4.69;
+let rateUSD = 4.39;
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let amount = +amountElement.value;
+  let currency = currencyElement.value;
+
+  let result;
+
+  switch (currency) {
+    case "EUR":
+      result = amount / rateEUR;
+      break;
+    case "USD":
+      result = amount / rateUSD;
+      break;
+  }
+
+  resultElement.innerText = result;
+});
